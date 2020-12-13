e64c9760711dfb2c0e3736c4ededc3deea6135d7
Drakodin <ttian31@gatech.edu>
2020-12-13 14:18:56 -0600

    Add ruining of fun leaderboard.

--- main.js
+++ main.js
@@ -5,17 +5,34 @@ const client = new discord.Client();
 const stems = require('./random');
 const fails = new stems.Fails();
 
+// Internal counter to designate the number.
 var counter = 1;
+
+// The highest number reached by the counting.
 var high_score = 0;
 
+// Maps users to their number of times ruining the count
+var lb = {};
+
+/**
+ * Only triggers if bot goes offline, which it shouldn't.
+ * The counter will reset in this case.
+ */
 client.on('ready', () => {
     counter = 1;
+    lb = {};
     console.log('Online!');
 })
 
 client.on('message', (msg) => {
     if (msg.content === 'c!num') {
         msg.channel.send(`The current number is ${counter}.`);
+    } else if (msg.content === 'c!lb') {
+        let board = "";
+        for (let key in Object.keys(lb)) {
+            board = board.concat(`${key}: ${lb[key]} ruins\n`);
+        }
+        msg.channel.send(`Here are the current stats\n: ${board}`);
     } else {
         var num = Number.parseInt(msg.content);
         if (Number.isInteger(num)) {
@@ -29,7 +46,7 @@ client.on('message', (msg) => {
                 }
             } else {
                 if (high_score < counter) {
-                    high_score = counter;
+                    high_score = counter - 1;
                 }
                 var stem = fails.failMsg();
                 msg.reply(`${stem} The current high score is ${high_score}.`);
